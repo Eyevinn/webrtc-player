b9fa368095fb0db78006ebae549cbe7a16367f75
David Nimon <dnimon@mentortek.com>
2023-11-17 12:48:57 -0600

    feat: add authorization header for whep, for stream key

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,7 @@ export class WebRTCPlayer extends EventEmitter {
   private iceServers: RTCIceServer[];
   private debug: boolean;
   private channelUrl: URL = <URL>{};
+  private authKey?: string = undefined;
   private reconnectAttemptsLeft: number = RECONNECT_ATTEMPTS;
   private csaiManager?: CSAIManager;
   private adapter: Adapter = <Adapter>{};
@@ -93,8 +94,9 @@ export class WebRTCPlayer extends EventEmitter {
     }
   }
 
-  async load(channelUrl: URL) {
+  async load(channelUrl: URL, authKey: string | undefined = undefined) {
     this.channelUrl = channelUrl;
+    this.authKey = authKey;
     this.connect();
   }
 
@@ -229,14 +231,16 @@ export class WebRTCPlayer extends EventEmitter {
         this.peer,
         this.channelUrl,
         this.onErrorHandler.bind(this),
-        this.mediaConstraints
+        this.mediaConstraints,
+        this.authKey
       );
     } else if (this.adapterFactory) {
       this.adapter = this.adapterFactory(
         this.peer,
         this.channelUrl,
         this.onErrorHandler.bind(this),
-        this.mediaConstraints
+        this.mediaConstraints,
+        this.authKey
       );
     }
     if (!this.adapter) {
